d2720ef65a3f4055a674f80d9f767057ce53a6b6
Vaishnavi Srivastava <121241512+Antisocialbutterfly033@users.noreply.github.com>
2023-12-09 17:01:22 +0530

    Add files via upload

---
+++ task2 codsoft.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Number of subjects
+        System.out.print("Enter the number of subjects: ");
+        int numSubjects = scanner.nextInt();
+
+        // Initialize total marks
+        double totalMarks = 0;
+
+        // Input marks for each subject
+        for (int i = 1; i <= numSubjects; i++) {
+            System.out.print("Enter marks obtained in subject " + i + " (out of 100): ");
+            double marks = scanner.nextDouble();
+            totalMarks += marks;
+        }
+
+        // Calculate average percentage
+        double averagePercentage = totalMarks / numSubjects;
+
+        // Calculate grade
+        char grade = calculateGrade(averagePercentage);
+
+        // Display results
+        System.out.println("\nResults:");
+        System.out.println("Total Marks: " + totalMarks);
+        System.out.printf("Average Percentage: %.2f%%\n", averagePercentage);
+        System.out.println("Grade: " + grade);
+
+        scanner.close();
+    }
+
+    private static char calculateGrade(double averagePercentage) {
+        if (averagePercentage >= 90) {
+            return 'A';
+        } else if (averagePercentage >= 80) {
+            return 'B';
+        } else if (averagePercentage >= 70) {
+            return 'C';
+        } else if (averagePercentage >= 60) {
+            return 'D';
+        } else {
+            return 'F';
+        }
+    }
+}
